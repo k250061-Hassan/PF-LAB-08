b907ebda7966d59c2c573bfc6a91011a97744820
Hassan Ali Khan <k250061@nu.edu.pk>
2025-10-23 09:29:31 +0500

    Add files via upload

---
+++ Task1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+	
+	int i,j,space;
+	
+	for(i=1; i<=5; i++){
+		
+		printf("%d:",i);
+		for(j=1; j<=5; j++){
+			printf("%4d", i*j);
+		}
+		printf("\n");
+	
+	}
+	
+	
+	return 0;
+}
